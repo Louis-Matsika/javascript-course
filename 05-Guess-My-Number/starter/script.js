a7990cb7096617d56daba71fa239c28eb10ec4e9
Louis Matsika <louis.matsika@bbc.co.uk>
2025-12-03 17:42:35 +0000

    lesson 5

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const numberToGuess = document.querySelector('.number').textContent = "11";
+
+// document.querySelector('.guess').textContent = 1;
+
+document.querySelector('.check').addEventListener('click', function ()
+{
+    const guess = document.querySelector('.guess').value;
+
+    if(guess === numberToGuess){
+        document.querySelector('.message').textContent = "correct!";
+    } 
+    else 
+    {
+        document.querySelector('.message').textContent = "try again...";
+    }
+
+});
