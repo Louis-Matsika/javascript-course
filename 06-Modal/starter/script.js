fe0388f9844401d4c54561b704a39a1f2fc5091a
Louis Matsika <louis.matsika@bbc.co.uk>
2025-12-06 16:22:36 +0000

    lesson 6

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// set up
+const btnShowModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function escKeyPress() {
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'k') closeModal();
+  });
+}
+
+// main loop
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+
+escKeyPress();
