9e8643471e1cfdbb4882e941650782af4bc7a3ca
Louis Matsika <louis.matsika@bbc.co.uk>
2026-01-01 16:42:30 +0000

    update

--- projects/09-Data-Structures-Operators/starter/script.js
+++ projects/09-Data-Structures-Operators/starter/script.js
@@ -312,3 +312,37 @@ btn.addEventListener('click', function () {
 });
 
 document.body.append(btn);
+
+// String Methods Practice
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// expected output:
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+function airportFlights(data) {
+  const dataArr = data.split('+');
+  let airportFlights = [];
+
+  for (const i of dataArr) {
+    airportFlights.push(i.split(';'));
+  }
+
+  for (const i of airportFlights) {
+    console.log(
+      `${i[0].includes('Delayed') ? '🔴 ' : ''}${i[0]
+        .replaceAll('_', ' ')
+        .trim()}
+      from ${i[1].slice(0, 3).toUpperCase()} to ${i[2]
+        .slice(0, 3)
+        .toUpperCase()}
+      (${i[3].replace(':', 'h')})`
+    );
+  }
+}
+
+airportFlights(flights);
